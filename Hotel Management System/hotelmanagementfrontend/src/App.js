0a59a943b9bf56354e7a9c1433e154719ed632c4
PasinduPerera10 <pasinduperera9910@gmail.com>
2021-09-08 17:37:50 +0530

    third commit

--- Hotel Management System/hotelmanagementfrontend/src/App.js
+++ Hotel Management System/hotelmanagementfrontend/src/App.js
@@ -1,46 +1,18 @@
-/*import Header from './roommanagementcomponents/Header';
-import AddRoom from './roommanagementcomponents/AddRoom';
-
-import{BrowserRouter as Router, Route} from 'react-router-dom'
-
-
-function App() {
-  return (
-    <Router>
-    <div>
-      
-    <Header/>
-    <div class="content">
-
-<Route path="/add" exact component={AddRoom}/>
-</div>
-        
-    </div>
-    </Router>
-  );
-}
-
-
-
-export default App;*/
-
-
-
 import React, { Component } from 'react';
 import{BrowserRouter as Router, Route} from 'react-router-dom'
-import AddRoom from './roommanagementcomponents/AddRoom';
-import EditRoom from './roommanagementcomponents/EditRoom';
-import AvailableRooms from './roommanagementcomponents/AvailableRooms';
-import RoomsForBooking from './roommanagementcomponents/RoomsForBooking';
-import Home from './roommanagementcomponents/Home';
-import Header from './roommanagementcomponents/Header';
-import backgroundimage from './roommanagementcomponents/backgroundimage';
-import PostDetails from './roommanagementcomponents/PostDetails';
-import RoomsCategory from './roommanagementcomponents/RoomsCategory';
-import Clock from './roommanagementcomponents/Clock';
-import AvailableLuxuryRooms from './roommanagementcomponents/AvailableLuxuryRooms';
-import footer from './roommanagementcomponents/footer';
-import RoomMonthlyReport from './roommanagementcomponents/RoomMonthlyReport';
+import AddRoom from './hotelmanagementcomponents/AddRoom';
+import EditRoom from './hotelmanagementcomponents/EditRoom';
+import AvailableRooms from './hotelmanagementcomponents/AvailableRooms';
+import RoomsForBooking from './hotelmanagementcomponents/RoomsForBooking';
+import Home from './hotelmanagementcomponents/Home';
+import Header from './hotelmanagementcomponents/Header';
+import backgroundimage from './hotelmanagementcomponents/backgroundimage';
+import PostRoomDetails from './hotelmanagementcomponents/PostRoomDetails';
+import RoomsCategory from './hotelmanagementcomponents/RoomsCategory';
+import Clock from './hotelmanagementcomponents/Clock';
+import AvailableLuxuryRooms from './hotelmanagementcomponents/AvailableLuxuryRooms';
+import footer from './hotelmanagementcomponents/footer';
+import RoomMonthlyReport from './hotelmanagementcomponents/RoomMonthlyReport';
 import 'antd/dist/antd.css';
 export default class App extends Component {
 
@@ -57,7 +29,7 @@ return (
 <Route path="/c" exact component={RoomsForBooking}></Route>
 <Route path="/add" exact component={AddRoom}></Route>
 <Route path="/edit/:id" exact component={EditRoom}></Route>
-  <Route path="/room/:id" exact component={PostDetails}></Route>
+  <Route path="/room/:id" exact component={PostRoomDetails}></Route>
   <Route path="/clock" exact component={Clock}></Route>
   <Route path="/category" exact component={RoomsCategory}></Route>
     <Route path="/AvailableLuxuryRooms" exact component={AvailableLuxuryRooms}></Route>
